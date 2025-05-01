97beb7508590944cdd8063838aa0f7f99c758781
JCurcio27 <jcurcio@tulane.edu>
2025-04-30 23:42:02 -0500

    Add files via upload

--- answers.md
+++ answers.md
@@ -2,8 +2,8 @@
 
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:** Jaedan Curcio
+
 
 
 Place all written answers from `recitation-08.md` here for easier grading.
@@ -12,7 +12,11 @@ Place all written answers from `recitation-08.md` here for easier grading.
 
 - **1b)**
 
+  - This algorithm is an implementation of Dijkstra's algorithm modified to track the number of edges in the path in order to return the path with smallest edge weight and least edges.
+The tracking/comparison of the edges itself takes place in constant time, and the rest of the algorithm has the same work as Dijstra's, so the work is O(|E| log(|E|)), since heap operations are performed once per each edge. Since
+the heap operations cannot be parallelized, the span is also O(|E| log(|E|))
 
 
-- **2b)**
+
+
 
--- main.py
+++ main.py
@@ -13,19 +13,46 @@ def shortest_shortest_path(graph, source):
       (shortest path weight, shortest path number of edges). See test case for example.
     """
     ### TODO
-    pass
+    heap = [(0, 0, source)]
+
+    shortest_path_info = {}
+
+    while heap:
+        dist, edges, node = heappop(heap)
+
+        if node in shortest_path_info:
+            continue
+
+        shortest_path_info[node] = (dist, edges)
+
+        for neighbor, weight in graph.get(node, []):
+            if neighbor not in shortest_path_info:
+                heappush(heap, (dist + weight, edges + 1, neighbor))
+
+    return shortest_path_info
+
     
 
     
-    
+
 def bfs_path(graph, source):
     """
     Returns:
-      a dict where each key is a vertex and the value is the parent of 
+      a dict where each key is a vertex and the value is the parent of
       that vertex in the shortest path tree.
     """
     ###TODO
-    pass
+    parents = {source: None}
+    queue = deque([source])
+
+    while queue:
+        curr = queue.popleft()
+        for neighbor in graph.get(curr, set()):
+            if neighbor not in parents:
+                parents[neighbor] = curr
+                queue.append(neighbor)
+
+    return parents
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -36,13 +63,20 @@ def get_sample_graph():
             }
 
 
-    
+
 def get_path(parents, destination):
     """
     Returns:
-      The shortest path from the source node to this destination node 
+      The shortest path from the source node to this destination node
       (excluding the destination node itself). See test_get_path for an example.
     """
     ###TODO
-    pass
+    path = []
+    curr = destination
+
+    while curr in parents and parents[curr] is not None:
+        path.append(parents[curr])
+        current = parents[curr]
+
+    return ''.join(reversed(path))
 
